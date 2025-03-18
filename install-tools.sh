400f40d36ed602d70082a029247442cc18cec68d
Martynas Zilinskas <martynas.zil@outlook.com>
2025-03-18 18:12:16 +0200

    Enhance installation script with logging and error handling

--- install-tools.sh
+++ install-tools.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 # For debian-12
 
+set -e
+
+log() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log "Starting the installation script."
+
 # yq
+log "Installing yq..."
 wget https://github.com/mikefarah/yq/releases/download/v4.44.2/yq_linux_amd64 -O /usr/bin/yq\
 && chmod +x /usr/bin/yq
+log "yq installed successfully."
 
 # jq
+log "Installing jq..."
 wget https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64 -O /usr/bin/jq\
 && chmod +x /usr/bin/jq
+log "jq installed successfully."
 
 # git
+log "Installing git..."
 sudo apt install git -y
+log "git installed successfully."
 
 # docker
+log "Setting up Docker repository..."
 ## Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
@@ -27,9 +42,18 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
+log "Installing Docker..."
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+log "Docker installed successfully."
 
 # azure cli 
+log "Installing Azure CLI..."
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+log "Azure CLI installed successfully."
+
 ## Extensions
+log "Adding Azure DevOps extension to Azure CLI..."
 az extension add --name azure-devops
+log "Azure DevOps extension added successfully."
+
+log "Installation script completed."
